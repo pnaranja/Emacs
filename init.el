8a30f8b6fb5e330a03b36e15219db4ac502e2d58
Paul Naranja <pnaranja@chegg.com>
2019-10-01 12:02:20 -0700

    Updating entries

--- init.el
+++ init.el
@@ -40,6 +40,10 @@
     lsp-mode
     lsp-ui
 
+    ;; Company
+    company
+    company-lsp
+
     ;; Dockerfile Mode
     dockerfile-mode
 
@@ -168,10 +172,20 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))))
+    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(package-selected-packages
+   (quote
+    (company-lsp company yaml-mode smex rg find-file-in-project exec-path-from-shell dockerfile-mode color-theme-sanityinc-tomorrow))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Company settings
+(add-hook 'after-init-hook 'global-company-mode)
+(require 'company-lsp)
+(push 'company-lsp company-backends)
+(setq company-lsp-async 1)
+(setq company-lsp-enable-recompletion 1)
